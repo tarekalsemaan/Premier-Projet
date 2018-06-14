728bd9a0d4e0eeff488e6abd6ccb9d9b3427607c
tarekalsemaan <tarekalsemaan@gmail.com>
2018-06-14 10:00:44 -0400

    Second commit

---
+++ factorielle.java
@@ -0,0 +1,19 @@
+
+public class factorielle {
+    public static void main(String[] args) {
+        int factoriel = 1;
+        int valeur;
+        valeur = Integer.parseInt(args[0]);
+        while (valeur > 1) {
+               factoriel = factoriel * valeur;
+               valeur = valeur - 1;
+        }
+        System.out.println("La factorielle est : " + factoriel);
+    }
+}
+git init
+git status
+git add factorielle.java
+git commit -m "Second commit"
+git push
+
